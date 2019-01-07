cb0927ad68cc5eb7ad09de213d5845705e5c76ce
Tony Bradley <tonybradley@Tonys-Mac-mini.fios-router.home>
2019-01-06 19:11:32 -0500

    4.0.0

--- index.js
+++ index.js
@@ -12,31 +12,29 @@ module.exports = function createEffectsDriver (simulate) {
         const mock = typeof simulate === 'object' && (simulate[config.tag] || simulate[run.name]);
 
         (mock || run)(...args)
-          .then(value => {
-            emitter.emit(config.tag, { value, error: null })
-          })
-          .catch(error => {
-            emitter.emit(config.tag, { value: null, error })
-          })
+          .then(value => emitter.emit(config.tag, { value, error: null }))
+          .catch(error => emitter.emit(config.tag, { value: null, error }))
       },
       complete: function () {
         unsub()
       }
     })
 
-    return function (tag) {
-      let sub
-      return xs.create({
-        start: function (listener) {
-          sub = e => {
-            listener.next(e)
+    return {
+      select: function (tag) {
+        let sub
+        return xs.create({
+          start: function (listener) {
+            sub = e => {
+              listener.next(e)
+            }
+            emitter.on(tag, sub)
+          },
+          stop: function () {
+            emitter.off(tag, sub)
           }
-          emitter.on(tag, sub)
-        },
-        stop: function () {
-          emitter.off(tag, sub)
-        }
-      })
+        })
+      }
     }
   }
 }
